d3b276bd2e75ff1f9d5f3a8cbc51ff492e5432b6
Berat Navruz <navruzberat8308@gmail.com>
2024-01-28 22:07:37 +0300

    argocd

--- uygulama.py
+++ uygulama.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
-app = Flask(name)
+app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return 'Merhaba, Docker ve Python!'
 
-if name == 'main':
+if __name__ == 'main':
     app.run(debug=True,host='0.0.0.0')
